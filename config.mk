fbec6772a60e83d7ec8acf2a1298d01045a6b22a
AnierinB <anierinb@evolution-x.org>
2020-04-04 22:10:02 +0000

    Init vendor_oneplus_camera
    - OnePlusCamera: v3.8.113
    - OnePlusCameraService: v2.2.12
    - OnePlusGallery: v3.8.21
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

---
+++ config.mk
@@ -0,0 +1,8 @@
+PRODUCT_PACKAGES += \
+    OnePlusCamera \
+    OnePlusCameraService \
+    OnePlusGallery
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml
