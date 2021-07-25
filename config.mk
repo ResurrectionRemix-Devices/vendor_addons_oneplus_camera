4e10694ed874efd39739899295e0d0623477d98a
Bush-cat <enzo.exel@gmail.com>
2021-07-25 14:16:09 +0200

    add OOS camera support to all builds
    less work required to install OOS camera in vanilla builds.

--- config.mk
+++ config.mk
@@ -1,7 +1,9 @@
+ifeq ($(TARGET_SHIPS_OOSCAM),true)
 PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusCameraService \
     OnePlusGallery
+endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
